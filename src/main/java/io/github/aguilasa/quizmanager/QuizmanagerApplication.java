c0211bca8af5428e8411f46d4bfaf04ea02aaf74
Ingmar Aguiar <ingmar.aguiar@gmail.com>
2025-06-29 21:05:00 -0300

    init: create Spring Boot quiz manager project with basic configuration and dependencies

---
+++ src/main/java/io/github/aguilasa/quizmanager/QuizmanagerApplication.java
@@ -0,0 +1,13 @@
+package io.github.aguilasa.quizmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/aguilasa/quizmanager/QuizmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aguilasa.quizmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
